3d0bd39bfe1a163c45bc47d2ac31a88732fb95d0
jini <haotian.jin@hotmail.com>
2025-08-16 19:33:32 +0800

    test

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def predict():
-    return 'predict!!!'
+def hello_world():
+    return '欢迎使用微信云托管！'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
